656711e142e08c03d247f2ab45794a0b6a00a4db
chakresh <chakresh@acis.io>
2020-05-14 01:49:33 +0530

    with test cases

--- src/main/java/com/example/Test/LeapYearCheck.java
+++ src/main/java/com/example/Test/LeapYearCheck.java
@@ -3,26 +3,27 @@ package com.example.Test;
 public class LeapYearCheck {
 
 	public boolean isLeapYear(int year) {
-		if (year % 100 == 0) {
-			if (year % 400 == 0) {
+		if (year > 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					return true;
+				} else {
+					return false;
+				}
+			} else if (year % 4 == 0) {
 				return true;
 			} else {
 				return false;
 			}
-		} else if (year % 4 == 0) {
-			return true;
 		} else {
 			return false;
 		}
 
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LeapYearCheck leapYear = new LeapYearCheck();
-		int year = 2008;
-		boolean res = leapYear.isLeapYear(year);
-		System.out.println(res);
-	}
+//	public static void main(String[] args) {
+//		LeapYearCheck leapYear = new LeapYearCheck();
+//		System.out.println(leapYear.isLeapYear(0));
+//	}
 
 }
--- src/test/java/com/example/Test/TestApplicationTests.java
+++ src/test/java/com/example/Test/TestApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,8 +12,36 @@ class TestApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void check_With_Number_Divided_BYFour_and_Hundred() {
+		LeapYearCheck leapYear = new LeapYearCheck();
+		assertTrue(leapYear.isLeapYear(2000));
+	}
+
+	@Test
+	void check_With_Number_Divided_BY_FourHundred_and_Hundred() {
+		LeapYearCheck leapYear = new LeapYearCheck();
+		assertTrue(leapYear.isLeapYear(1600));
+	}
+
+	@Test
+	void check_With_Number_Divided_BY_Hundredand_NotDivideBy_FourHundred() {
+		LeapYearCheck leapYear = new LeapYearCheck();
+		assertEquals(leapYear.isLeapYear(1500), false);
+	}
+
+	@Test
+	void check_With_NotDivideBy_Four() {
+		LeapYearCheck leapYear = new LeapYearCheck();
+		assertEquals(leapYear.isLeapYear(2007), false);
+	}
 	
-	
-	
+	//check for Zero
+	@Test
+	void check_With_wrongInput() {
+		LeapYearCheck leapYear = new LeapYearCheck();
+		assertEquals(leapYear.isLeapYear(0), false);
+	}
 
 }
